011eb31ff39cb3270aaa34f867c7aca40821eaf9
Erastuskanyatta <erastusmuriithi146@gmail.com>
2025-02-06 14:50:51 +0300

    clean ups.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import './App.css';
-import './styles.css';
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Login from './components/shared/auth/authenticate/Login';
 import Signup from './components/shared/auth/register/Signup'
 import ForgetPassword from './components/shared/auth/forgotPassword/ForgetPassword'
@@ -11,25 +9,28 @@ import VerifyUser from './components/shared/auth/verifyUser/VerifyUser'
 import RegisterComplete from './components/shared/auth/registerComplete/RegisterComplete';
 import LandingPage from './components/core/landing/LandingPage';
 
+import './App.css';
+import './styles.css';
+
 const App = () => {
   return (
     <>
-    <Router>
-      <Routes>
+      <Router>
+        <Routes>
 
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgetPassword" element={<ForgetPassword />}/>
-        <Route path="/resetPassword" element={<ResetPassword />}/>
-        <Route path="/verifyUser" element={<VerifyUser />}/>
-        <Route path="/registerComplete" element={<RegisterComplete />}/>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgetPassword" element={<ForgetPassword />} />
+          <Route path="/resetPassword" element={<ResetPassword />} />
+          <Route path="/verifyUser" element={<VerifyUser />} />
+          <Route path="/registerComplete" element={<RegisterComplete />} />
 
-        <Route path="/landingpage" element={<LandingPage/>} />
-      </Routes>
-    </Router>
+          <Route path="/landingpage" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </>
   );
 };
 
- export default App;
+export default App;
--- src/components/shared/auth/forgotPassword/ForgetPassword.jsx
+++ src/components/shared/auth/forgotPassword/ForgetPassword.jsx
@@ -3,10 +3,9 @@ import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import apiService from '../../../../services/ApiService';
+import email_icon from '../../../assets/email.png';
 
 import './ForgetPassword.css';
-
-import email_icon from '../../../assets/email.png';
 
 const ForgetPassword = () => {
     const [message, setMessage] = useState('');
